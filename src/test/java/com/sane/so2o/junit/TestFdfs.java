1de07bed126c15c8e1fcb880d75d98870f88ca04
muyushan <muyushanm@126.com>
2020-07-06 07:17:09 +0800

    增加FastFdfsd 的客户端访问2

--- src/test/java/com/sane/so2o/junit/TestFdfs.java
+++ src/test/java/com/sane/so2o/junit/TestFdfs.java
@@ -31,10 +31,14 @@ public class TestFdfs extends BaseTest {
 
         // 上传   （文件上传可不填文件信息，填入null即可）
         try{
-            StorePath storePath = fastFileStorageClient.uploadFile(inputStream, file.length(), FilenameUtils.getExtension(file.getName()), mataData);
-            System.out.println(storePath.getFullPath());
-            System.out.println(storePath.getGroup());
-            System.out.println(storePath.getPath());
+//            StorePath storePath = fastFileStorageClient.uploadFile(inputStream, file.length(), FilenameUtils.getExtension(file.getName()), mataData);
+//            System.out.println(storePath.getFullPath());
+//            System.out.println(storePath.getGroup());
+//            System.out.println(storePath.getPath());
+            StorePath path=fastFileStorageClient.uploadImageAndCrtThumbImage(inputStream,file.length(),FilenameUtils.getExtension(file.getName()), mataData);
+            System.out.println(path.getFullPath());
+            System.out.println(path.getGroup());
+            System.out.println(path.getPath());
         }catch(Exception exception){
             System.out.println(exception.getMessage());
             exception.printStackTrace();;
